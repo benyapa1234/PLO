0f996c6c7267ce2c5448f258799af31039feaacb
benyapa1234 <aorloveyou44@gmail.com>
2024-06-05 09:48:12 +0700

    update

--- seniorproject/src/App.jsx
+++ seniorproject/src/App.jsx
@@ -8,10 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+      <div class = "container">
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
